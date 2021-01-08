2349569504244537de606997f820643873f00c89
qnrjs42 <qnrjs42@naver.com>
2021-01-09 00:20:30 +0900

    4-11_16. flex 실전 반응형 웹사이트 제작 - Testimonial Section, Blog Section

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,7 @@
-$(function(){
-  
-  // Trigger
-  $('.trigger').click(function(){
-    $(this).toggleClass('active')
-    $('.gnb').toggleClass('active')
-  })
-  $('.gnb a').click(function(){
-    $('.gnb, .trigger').removeClass('active')
-  })
-  
-  // Sliding jQuery
-  $('.gnb a, .gototop').click(function(e){
-    $.scrollTo(this.hash || 0, 800);
-    e.preventDefault();
-  })
-  
-  // Header Scroll Change
-  $(window).scroll(function(){
-    if($(window).scrollTop() > 50) {
-      $('header, .gototop').addClass('active')
-    }
-    else {
-      $('header, .gototop').removeClass('active')
-    }    
-  })
-  
+$(function() {
   // Slick.js
-  $('.myslider').slick({
+  $(".myslider").slick({
+    // 1024px 이상
     dots: true,
     infinite: false,
     speed: 300,
@@ -34,31 +9,34 @@ $(function(){
     slidesToScroll: 3,
     responsive: [
       {
+        // 1024px
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
+        // 600px
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
-        }
+          slidesToScroll: 2,
+        },
       },
       {
+        // 480px
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
+          slidesToScroll: 1,
+        },
+      },
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
-    ]
+    ],
   });
 })
